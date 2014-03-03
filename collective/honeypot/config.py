bd3ceb0b6d59564e4756b57be3ca1326bdd96676
Maurits van Rees <maurits@vanrees.org>
2014-03-03 23:29:31 +0100

    Explain extra protected actions.

--- collective/honeypot/config.py
+++ collective/honeypot/config.py
@@ -1,13 +1,12 @@
 # Field that MAY be in some requests, but MUST be empty.
 HONEYPOT_FIELD = 'protected_1'
-# Currently, these actions are protected, meaning: the forms have been
-# changed to add the above field.  If you add actions here but do not
-# change the forms, they become unusable for visitors, which is not
-# what you want.
+# Currently, these actions are extra protected by making the honeypot
+# field required, though it of course still needs to be empty.  If you
+# add actions here but do not change the forms, they become unusable
+# for visitors, which is not what you want.  On the other hand, if you
+# have a form that you no longer wish to use, you can add it here and
+# it will stop functioning.
 PROTECTED_ACTIONS = (
-    'discussion_reply',
-    'join_form',
-    'sendto_form',
     )
 # Actions that are not checked:
 WHITELISTED_ACTIONS = (
