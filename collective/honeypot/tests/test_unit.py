133fb226801406464d8c791b8e7401b7e6985b2b
Maurits van Rees <maurits@vanrees.org>
2014-03-03 21:57:26 +0100

    Add tests for get_form.

--- collective/honeypot/tests/test_unit.py
+++ collective/honeypot/tests/test_unit.py
@@ -2,9 +2,11 @@
 
 import unittest
 
+from zope.publisher.browser import TestRequest
 from collective.honeypot.config import FORBIDDEN_HONEYPOT_FIELD
 from collective.honeypot.config import REQUIRED_HONEYPOT_FIELD
 from collective.honeypot.utils import found_honeypot
+from collective.honeypot.utils import get_form
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -28,3 +30,24 @@ class UtilsTestCase(unittest.TestCase):
         self.assertEqual(found_honeypot(
             {REQUIRED_HONEYPOT_FIELD: 'hello',
              FORBIDDEN_HONEYPOT_FIELD: 'hello'}), 'has forbidden field')
+
+    def test_get_form(self):
+        # get_form gets the form fields, leaving out password fields.
+        self.assertEqual(get_form({}), {})
+        self.assertEqual(get_form({'foo': 'bar'}), {'foo': 'bar'})
+        self.assertEqual(get_form(TestRequest()), {})
+        self.assertEqual(get_form(TestRequest(form={'hoppa': 'kikoo'})),
+                         {'hoppa': 'kikoo'})
+        secret_request = TestRequest(
+            form={'hoppa': 'kikoo',
+                  'password': 'secret',
+                  'password_confirm': 'secret2',
+                  '__ac_password': 'secret3',
+                  '__ac_name': 'admin',
+                  })
+        self.assertEqual(get_form(secret_request),
+            {'hoppa': 'kikoo', '__ac_name': 'admin'})
+        # The stripped data should still be on the original form.
+        self.assertEqual(secret_request.form['password'], 'secret')
+        self.assertEqual(secret_request.form['password_confirm'], 'secret2')
+        self.assertEqual(secret_request.form['__ac_password'], 'secret3')
