99db4c4bbc35698e7c6977013612ad951f9387f4
Maurits van Rees <maurits@vanrees.org>
2014-04-03 00:08:02 +0200

    Load the z3c.jbot overrides for Plone 3.

--- collective/honeypot/plone3/patches.py
+++ collective/honeypot/plone3/patches.py
@@ -4,3 +4,15 @@
 from AccessControl import allow_class
 from zExceptions import Forbidden
 allow_class(Forbidden)
+
+from collective.honeypot import config
+
+protected = [
+#     'sendto_form',
+#     'sendto',
+     'contact-info',
+#     'send_feedback_site',
+#     'register',
+    ]
+# Explicitly add the actions that we protect.
+config.EXTRA_PROTECTED_ACTIONS.update(protected)
