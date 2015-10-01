7a9db69c0d7cfc2ecfe9d032ae7b58111a3920bf
Maurits van Rees <maurits@vanrees.org>
2015-10-01 05:58:52 +0200

    Back to development: 1.0.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='collective.honeypot',
-      version='1.0.2',
+      version='1.0.3.dev0',
       description="Anti-spam honeypot for Plone",
       long_description=(open("README.rst").read() + "\n" +
                         open("CHANGES.rst").read()),
